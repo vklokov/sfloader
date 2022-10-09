fcbe39cce5f7273bc55661b9938184748d372324
Vladimir Klokov <r33z@nismo.local>
2022-10-09 12:47:49 +0200

    cleanup

--- sfloader/sfloader.py
+++ sfloader/sfloader.py
@@ -2,8 +2,6 @@ from sfloader.credentials import Credentials
 from sfloader.job import Job
 
 class SForce:
-    job = None
-
     def __init__(self, grant_type, client_id, client_secret,
                  username, password, api_version, host,
     ):
@@ -15,10 +13,10 @@ class SForce:
         self.credentials.retrieve()
 
     def upload(self, file, object_type, report_builder, external_key, operation, line_ending):
-        self.job = Job(credentials=self.credentials, report_builder=report_builder)
-        self.job.create(object_type=object_type, operation=operation,
+        job = Job(credentials=self.credentials, report_builder=report_builder)
+        job.create(object_type=object_type, operation=operation,
                         line_ending=line_ending, external_key=external_key)
-        self.job.upload_file(file)
-        self.job.finalize()
-        self.job.check_status()
+        job.upload_file(file)
+        job.finalize()
+        job.check_status()
 
