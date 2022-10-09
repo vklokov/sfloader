f3093fda7cbf3758fb9946e2f6ae739ec3c62056
Vladimir Klokov <r33z@nismo.local>
2022-10-09 13:05:04 +0200

    make reports be accessible outside repo

--- sfloader/__init__.py
+++ sfloader/__init__.py
@@ -1,3 +1,5 @@
 from sfloader import SFLoader
+from sfloader.reports.io_report import IOReport
+from sfloader.reports.file_report import FileReport
 
-__ALL__ = [SFLoader]
+__ALL__ = [SFLoader, IOReport, FileReport]
